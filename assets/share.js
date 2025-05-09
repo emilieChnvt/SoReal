78030b22d874cd7ea35cddc98645b9b027ab7a27
Emilie <emilie.chnvt@gmail.com>
2025-05-09 17:07:48 +0200

    shared functionnality added

---
+++ assets/share.js
@@ -0,0 +1,18 @@
+document.querySelectorAll(".share-button").forEach((btn) => {
+    btn.addEventListener("click", async () => {
+        const shareData = {
+            title: btn.dataset.title,
+            text: btn.dataset.text,
+            url: btn.dataset.url
+        };
+
+        const resultPara = btn.closest("div").querySelector(".result");
+
+        try {
+            await navigator.share(shareData);
+            if (resultPara) resultPara.textContent = "Partagé avec succès !";
+        } catch (err) {
+            if (resultPara) resultPara.textContent = `Erreur : ${err}`;
+        }
+    });
+});
