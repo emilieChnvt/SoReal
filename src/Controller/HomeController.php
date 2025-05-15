82ecba872d16c5dc3b98d4a55223116d54157100
Emilie <emilie.chnvt@gmail.com>
2025-05-15 19:59:11 +0200

    rules added to not edit image profile from others

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,14 +10,29 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index(MailerInterface $mailer): Response
     {
-        return new Response('<h1>Bienvenue sur SoReal !</h1>');
+        $email = (new Email())
+            ->from('hellooooo@example.com')
+            ->to('yooooou@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!');
+         //   ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+        return $this->redirectToRoute('app_posts');
+
     }
 
 
